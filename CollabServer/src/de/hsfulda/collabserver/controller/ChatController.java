321ecafc3ea207462493d5e296406e7d420566b6
Johannes Mueller <dev@straight-shoota.de>
2012-01-19 23:33:25 +0100

    URLs in chat messages are converted to links

--- CollabServer/src/de/hsfulda/collabserver/controller/ChatController.java
+++ CollabServer/src/de/hsfulda/collabserver/controller/ChatController.java
@@ -6,20 +6,33 @@ import org.jsoup.safety.Whitelist;
 import de.hsfulda.collabserver.CollabClient;
 import de.hsfulda.collabserver.Message;
 
-public class ChatController extends ActionDelegateController {	
+public class ChatController extends ActionDelegateController {
 	@Override
-	protected void initActions(){
+	protected void initActions() {
 		bind("message", new CollabMessageListener() {
 			@Override
-			public void doAction(Message message, CollabClient client) throws Exception {
+			public void doAction(Message message, CollabClient client)
+					throws Exception {
 				String content;
 				content = message.getContent().getString("text");
 				content = Jsoup.clean(content, Whitelist.simpleText());
+
+				// content.replaceAll("(.*://[^<>[:space:]]+[[:alnum:]/])",
+				// "<a href=\"$1\">HereWasAnURL</a>");
+
+				content = content.replaceAll("(\\A|\\s)(www\\.\\S+)(\\s|\\z)",
+						"$1http://$2$3");
+
+				content = content.replaceAll(
+						"(\\A|\\s)((http|https|ftp|mailto):\\S+)(\\s|\\z)",
+						"$1<a href=\"$2\">$2</a>$4");
+
 				ChatMessage chatMessage = new ChatMessage(client, content);
-				//chatLog.add(cm);
+				// chatLog.add(cm);
 				client.getSession().getLog().append(chatMessage);
-				System.out.println(chatMessage);
+				// System.out.println(chatMessage);
 				client.getSession().send("chat.message", chatMessage);
+
 			}
 		});
 	}
